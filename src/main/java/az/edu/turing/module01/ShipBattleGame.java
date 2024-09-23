07ee4d04e72d6a9bbd53e362b3fb16776d06ccab
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2024-09-23 12:32:03 +0400

    implemented requested changes: added requested methods

--- src/main/java/az/edu/turing/module01/ShipBattleGame.java
+++ src/main/java/az/edu/turing/module01/ShipBattleGame.java
@@ -8,16 +8,8 @@ public class ShipBattleGame {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        // Size of the square
         final int SIZE = 5;
-        char[][] square = new char[SIZE][SIZE];
-
-        // Filling square
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                square[i][j] = '-';
-            }
-        }
+        char[][] square = initializeSquare(SIZE);
 
         int targetRow = random.nextInt(SIZE);
         int targetCol = random.nextInt(SIZE);
@@ -25,7 +17,6 @@ public class ShipBattleGame {
         System.out.println("All set. Get ready to rumble!");
 
         while (true) {
-            // Prompt is provided to the method to increase readability
             int row = getValidInput(scanner, "Enter the row number (1-5): ", SIZE);
             int col = getValidInput(scanner, "Enter the column number (1-5): ", SIZE);
 
@@ -49,28 +40,36 @@ public class ShipBattleGame {
         scanner.close();
     }
 
-    // Method to get valid input from user
     private static int getValidInput(Scanner scanner, String prompt, int max) {
-        int input = -1;
+        int input;
         while (true) {
             System.out.print(prompt);
             if (!scanner.hasNextInt()) {
                 System.out.println("Invalid input. Please enter a number.");
                 scanner.next();
-                continue;
+            } else {
+                input = scanner.nextInt();
+                if (input >= 1 && input <= max) {
+                    break;
+                } else System.out.println("Please enter a number between 1 and " + max);
             }
-            input = scanner.nextInt();
-            if (input < 1 || input > max) {
-                System.out.println("Please enter a number between 1 and " + max);
-                continue;
-            }
-            break;
         }
 
         return input;
     }
 
-    // Method to print game square
+    public static char[][] initializeSquare(int size) {
+        char[][] square = new char[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                square[i][j] = '-';
+            }
+        }
+
+        return square;
+    }
+
     private static void printGameSquare(char[][] square) {
         System.out.println("  0 | 1 | 2 | 3 | 4 | 5 |");
         for (int i = 0; i < square.length; i++) {
