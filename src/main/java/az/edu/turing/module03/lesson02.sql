0d40e82ac2fff1445c25adbbb2f8c20dd1de0bf7
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2024-11-03 08:01:28 -0800

    added required relations

--- src/main/java/az/edu/turing/module03/lesson02.sql
+++ src/main/java/az/edu/turing/module03/lesson02.sql
@@ -11,8 +11,8 @@ updated_by          bigint,
 email               varchar(50) UNIQUE NOT NULL,
 name                varchar(20) NOT NULL,
 surname             varchar(30) NOT NULL,
-display_name        varchar(40) NOT NULL,
-gender              varchar(10) NOT NULL,
+username            varchar(40) NOT NULL UNIQUE,
+gender              varchar(6) NOT NULL CHECK (gender IN ('Male', 'Female', 'Other')),
 age                 integer CHECK(age > 0),
 is_active           boolean                                 DEFAULT TRUE,
 PRIMARY KEY(ID),
@@ -22,9 +22,6 @@ CREATE TABLE IF NOT EXISTS posts(
 id                  bigserial,
 created_at          timestamptz(3)                          DEFAULT CURRENT_TIMESTAMP,
 updated_at          timestamptz(3)                          DEFAULT CURRENT_TIMESTAMP,
-like_count          integer CHECK(like_count >= 0)          DEFAULT 0,
-comment_count       integer CHECK(comment_count >= 0)       DEFAULT 0,
-share_count         integer CHECK(share_count >= 0)         DEFAULT 0,
 created_by          bigint,
 updated_by          bigint,
 description         varchar(255),
@@ -36,9 +33,6 @@ CREATE TABLE IF NOT EXISTS comments(
 id                  bigserial,
 created_at          timestamptz(3)                          DEFAULT CURRENT_TIMESTAMP,
 updated_at          timestamptz(3)                          DEFAULT CURRENT_TIMESTAMP,
-like_count          integer CHECK(like_count >= 0)          DEFAULT 0,
-dislike_count       integer CHECK(dislike_count >= 0)       DEFAULT 0,
-sub_comment_count   integer CHECK(sub_comment_count >= 0)   DEFAULT 0,
 created_by          bigint,
 updated_by          bigint,
 post_id             bigint,
@@ -48,41 +42,51 @@ CONSTRAINT fk_user FOREIGN KEY(created_by) REFERENCES users(id),
 CONSTRAINT fk_user2 FOREIGN KEY(updated_by) REFERENCES users(id),
 CONSTRAINT fk_post FOREIGN KEY(post_id) REFERENCES posts(id));
 
---i could only imagine likes table as many-to-many table.
-CREATE TABLE IF NOT EXISTS likes(
+CREATE TABLE IF NOT EXISTS sub_comments(
+id                  bigserial,
+comment_id          bigint,
+user_id             bigint,
+context             varchar(255),
+PRIMARY KEY(id),
+CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id),
+CONSTRAINT fk_comment FOREIGN KEY(comment_id) REFERENCES comments(id));
+
+CREATE TABLE IF NOT EXISTS post_likes(
 liked_by            bigint,
 post_id             bigint,
 PRIMARY KEY(liked_by, post_id));
 
-INSERT INTO users(name, surname, display_name, email, gender, age)
-VALUES ('Semmed', 'Kosayev', 'ForeverOyuncu2', 'foreveroyuncu2@gmail.com', 'MALE', 19);
+CREATE TABLE IF NOT EXISTS comment_likes(
+liked_by            bigint,
+comment_id          bigint,
+PRIMARY KEY(liked_by, comment_id));
+
+CREATE TABLE IF NOT EXISTS comment_dislikes(
+liked_by            bigint,
+comment_id          bigint,
+PRIMARY KEY(liked_by, comment_id));
+
+
+
+INSERT INTO users(name, surname, username, email, gender, age)
+VALUES ('Semmed', 'Kosayev', 'ForeverOyuncu2', 'foreveroyuncu2@gmail.com', 'Male', 19);
 
 INSERT INTO posts(created_by, description)
 VALUES (1, 'The first post of the account. Supporting is higly valued.');
 
-UPDATE posts
-SET like_count = 1
-WHERE created_by = 1;
-
-INSERT INTO likes(liked_by, post_id)
+INSERT INTO post_likes(liked_by, post_id)
 VALUES(1, 1);
 
 INSERT INTO comments(created_by, post_id, context)
 VALUES(1, 1, 'postu beyenib, yorum atanlar sagolsun');
 
+INSERT INTO sub_comments(comment_id, user_id, context)
+VALUES (1, 1, 'beyenmeyenlerinde cani sag olsun');
+
 SELECT * FROM users;
 SELECT * FROM posts;
 SELECT * FROM comments;
-SELECT * FROM likes;
-
---joining users and posts
-SELECT p.id, p.like_count, p.comment_count, p.share_count, p.description, u.display_name FROM posts p
-JOIN users u
-ON p.created_by = u.id;
-
---joining users, posts, and comments
-SELECT p.id, p.like_count, p.comment_count, p.share_count, p.description, u.display_name, c.context FROM posts p
-JOIN users u
-ON p.created_by = u.id
-JOIN comments c
-ON p.id = c.post_id;
+SELECT * FROM sub_comments;
+SELECT * FROM post_likes;
+SELECT * FROM comment_likes;
+SELECT * FROM comment_dislikes;
